72e0a5646f75a4a30e302ad6ecbdd3a046d13d05
malimba <malimbageorgeyandanbon@gmail.com>
2025-06-05 02:20:59 +0000

    initial file explorer functionality

--- backend/core/models.py
+++ backend/core/models.py
@@ -9,10 +9,14 @@ class Device(models.Model):
     ssh_port = models.IntegerField(default=22)
     ssh_username = models.CharField(max_length=100)
     ssh_password = models.CharField(max_length=128)  # You should encrypt this in production
-
+    root_path = models.CharField(max_length=256, blank=True)  # todo: dynamically set when device is registered
     created_at = models.DateTimeField(auto_now_add=True)
     last_seen = models.DateTimeField(null=True, blank=True)
 
+    def save(self, *args, **kwargs):
+        if not self.root_path:
+            self.root_path = f"/home/{self.user.username}" #assuminng the username used to login is the same as the user profile on the home machine
+        super().save(*args, **kwargs)
     def __str__(self):
         return f"{self.name} ({self.device_id})"
 
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import CustomTokenObtainPairView, RegisterDeviceView, FileListView, LogoutView
+from .views import CustomTokenObtainPairView, RegisterDeviceView, FileListView, LogoutView, DeviceFileDownloadView
 
 urlpatterns = [
     path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('register_device/', RegisterDeviceView.as_view()),
-    path('files/', FileListView.as_view()),
+    path('devices/<str:device_id>/files/', FileListView.as_view()),
+    path("devices/<str:device_id>/files/download/", DeviceFileDownloadView.as_view(), name="device-file-download"),
+
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
